8314b38a7190be283ac5127e982416c239826d84
Narinthon <narinthon.tanwi@gmail.com>
2026-02-18 20:47:50 +0700

    save to database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer # Model for Analyzing Sentiment
+from sqlalchemy import create_engine
 
 analyzer = SentimentIntensityAnalyzer()
 
@@ -49,10 +50,46 @@ def get_ticker_data(symbol, interval, period):
 
   return df_prices, df_news
 
+# postgresql://[user]:[password]@[host]:[port]/[db_name]
+db_url = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+engine = create_engine(db_url)
+
+def save_to_database(df_prices, df_news):
+   print("checking database")
+
+   # check if data already exists
+   try:
+      existing_prices = pd.read_sql("SELECT \"Symbol\", \"Datetime\" FROM stock_prices", engine)
+
+      df_prices['Datetime'] = pd.to_datetime(df_prices['Datetime'])
+      existing_prices['Datetime'] = pd.to_datetime(existing_prices['Datetime'])
+
+      new_prices = df_prices.merge(existing_prices, on=['Symbol', 'Datetime'], how='left', indicator=True)
+      new_prices = new_prices[new_prices['_merge'] == 'left_only'].drop('_merge', axis=1)
+
+      # save to database
+      if not new_prices.empty:
+         new_prices.to_sql('stock_prices', engine, index=False, if_exists='append')
+         print(f"Saved {len(new_prices)} new prices to database")
+      else:
+         print("No new prices to save")
+  
+   # save to database
+   except Exception as e:
+      print(f"System Message: {e}")
+      print(f"First time saving data to database")
+      df_prices.to_sql('stock_prices', engine, index=False, if_exists='append')
+      df_news.to_sql('stock_news', engine, index=False, if_exists='append')
+
 symbol = ['BBL.BK', 'SCB.BK', 'KTB.BK']
 interval = '1h'
 period = '7d'
 prices, news = get_ticker_data(symbol, interval, period)
-
 print(f"Total prices: {prices}")
 print(f"Total news: {news}")
+
+save_to_database(prices, news)
+
+print("\nTesting Data Read")
+test_df = pd.read_sql("SELECT * FROM stock_news LIMIT 5", engine)
+print(test_df)
